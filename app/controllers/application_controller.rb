a8b0a1f854e9bdf2690844256d02b5e630aea515
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-08-01 16:49:31 -0700

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,49 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  get '/reviews' do
+    reviews = Review.all.order(:title).limit(20)
+    reviews.to_json
+  end
+
+  get '/reviews/:id' do
+    reviews = Review.find(params[:id])
+    reviews.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+
+    review.to_json
+  end
+
+
+  # Delete request
+  delete '/reviews/:id' do
+    # find the review using the ID
+    # delete the review
+    # send a response with the deleted review as JSON
+    
+    review = Review.find(params[:id])
+    review.destroy
+  end
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -15,5 +59,4 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
-
 end
